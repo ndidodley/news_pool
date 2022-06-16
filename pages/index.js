b46630f80aede362cff15d23c13c6edf87f2521a
Ndi Dodley <ndidodley@gmail.com>
2022-06-16 02:10:13 +0100

    Initilialised the base project and system files

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-screen h-screen bg-red-600'>
+      <h1 className='font-bold text-gray-50'>Hello World</h1>
+    </div>
+  )
+}
